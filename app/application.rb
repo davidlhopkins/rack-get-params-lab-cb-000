c30932765db72679e3672d74cc510b000e211fed
David Hopkins <dlhopkins2@gmail.com>
2018-07-13 02:28:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
-      resp.write "Your cart is empty"
+      resp.write "Path Not Found"
     end
 
     resp.finish
